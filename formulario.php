6b3d6d64e8d54fb598638805edfa87645ac0ca8e
Wilson <wilsonrueda2@gmail.com>
2020-08-31 00:57:19 -0500

    inicio de aplicacion juego

---
+++ formulario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/index.css">
+    <box-icon type='solid' name='home'></box-icon>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="./imagenes/bxs-home.svg" alt="">
+            <a href="index.html">Home</a>
+        </nav>
+    </header>
+    <form>
+        <label for="Nombre">Nombre</label><br>
+        <input type="text"><br>
+        <label for="Nombre">Correo</label><br>
+        <input type="email"><br>
+        <label for="Nombre">Contraseña</label><br>
+        <input type="password"><br>
+        <label for="Nombre">Repetir contraseña</label><br>
+        <input type="password"><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
